4d21b924d65f69e0ee27e3ce9d90731b452ce011
Chase <slatterychase@gmail.com>
2020-07-08 20:16:32 -0400

    Heap based Astar with units

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IHeapItem<Node>
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    int heapIndex;
+
+
+
+    public Node(bool walk, Vector3 pos, int gX, int gY)
+    {
+        gridX = gX;
+        gridY = gY;
+        walkable = walk;
+        worldPosition = pos;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+         return heapIndex;
+        }
+        set
+        {
+            heapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+        if(compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+        return -compare;
+    }
+}
+
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+using System;
+
+public class Pathfinding : MonoBehaviour
+{
+    PathRequestManager requestManager;
+    Grid grid;
+    public int diagonalValue;
+    public int straightValue;
+
+    private void Awake()
+    {
+        grid = GetComponent<Grid>();
+        requestManager = GetComponent<PathRequestManager>();
+    }
+
+    public void StartFindPath(Vector3 startPosition, Vector3 targetPosition)
+    {
+        StartCoroutine(FindPath(startPosition, targetPosition));
+    }
+    IEnumerator FindPath(Vector3 startPos, Vector3 targetPos)
+    {
+        Vector3[] waypoints = new Vector3[0];
+        bool pathSuccess = false;
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        if(startNode.walkable && targetNode.walkable) { 
+        Heap<Node> openSet = new Heap<Node>(grid.MaxHeapSize);
+        HashSet<Node> closedSet = new HashSet<Node>();
+
+        openSet.Add(startNode);
+
+            while (openSet.Count > 0)
+            {
+                Node currentNode = openSet.RemoveFirst();
+
+                closedSet.Add(currentNode);
+
+                if (currentNode == targetNode)
+                {
+                    sw.Stop();
+                    print(sw.ElapsedMilliseconds);
+                    pathSuccess = true;
+                    break;
+                }
+
+                foreach (Node neighbor in grid.GetNeighbors(currentNode))
+                {
+                    if (neighbor.walkable == false || closedSet.Contains(neighbor))
+                    {
+                        continue;
+                    }
+                    int newMovementCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
+
+                    if (newMovementCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor))
+                    {
+                        neighbor.gCost = newMovementCostToNeighbor;
+                        neighbor.hCost = GetDistance(neighbor, targetNode);
+
+                        neighbor.parent = currentNode;
+
+                        if (!openSet.Contains(neighbor))
+                        {
+                            openSet.Add(neighbor);
+                        }
+                    }
+                }
+
+            }
+        }
+        yield return null;
+        if (pathSuccess)
+        {
+            waypoints = RetracePath(startNode, targetNode);
+        }
+        requestManager.FinishedProcessingPath(waypoints, pathSuccess);
+    }
+    Vector3[] RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while(currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        Vector3[] waypoints = SimplifyPath(path);
+        Array.Reverse(waypoints);
+        return waypoints;
+
+    }
+
+    Vector3[] SimplifyPath(List<Node> path)
+    {
+        List<Vector3> waypoints = new List<Vector3>();
+        Vector2 directionOld = Vector2.zero;
+
+        for(int i = 1; i < path.Count; i++)
+        {
+            Vector2 directionNew = new Vector2(path[i - 1].gridX - path[i].gridX, path[i - 1].gridY - path[i].gridY);
+            if(directionNew != directionOld)
+            {
+                waypoints.Add(path[i].worldPosition);
+
+            }
+            directionOld = directionNew;
+        }
+        return waypoints.ToArray();
+        
+    }
+    public int GetDistance(Node nodeA, Node nodeB)
+    {
+        int distanceX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distanceY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if(distanceX > distanceY)
+        {
+            return diagonalValue * distanceY + straightValue * (distanceX - distanceY);
+        }
+        else
+        {
+            return diagonalValue * distanceX + straightValue * (distanceY - distanceX);
+        }
+    }
+}
